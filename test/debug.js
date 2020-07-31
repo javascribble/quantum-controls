719f267ab1ec4ed60bc1c1933d73c96a10f2eb06
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-07-31 09:15:29 -0400

    add test case

--- test/debug.js
+++ test/debug.js
@@ -1,4 +1,10 @@
 import '/node_modules/@javascribble/quantum/source/main.js';
 import '/source/main.js';
 
+const keyboard = document.querySelector('quantum-keyboard');
+keyboard.keys.set('Enter', {
+    up: console.log,
+    down: console.log
+});
+
 document.body.style.visibility = 'visible';
