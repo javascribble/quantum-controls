f50029621212a10ab3f468b5e5679959e3363993
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-07-31 09:11:49 -0400

    add handlers

--- source/elements/keyboard.js
+++ source/elements/keyboard.js
@@ -1,6 +1,30 @@
 export class Keyboard extends quantum.Component {
+    keys = new Map();
+
     constructor() {
         super();
+
+        // const keyPress = (event) => {
+        //     if (this.keys.has(event.key)) {
+        //         this.keys.get(event.key).press?.(event);
+        //     }
+        // };
+
+        const keyDown = (event) => {
+            if (this.keys.has(event.key)) {
+                this.keys.get(event.key).down?.(event);
+            }
+        };
+
+        const keyUp = (event) => {
+            if (this.keys.has(event.key)) {
+                this.keys.get(event.key).up?.(event);
+            }
+        };
+
+        //document.addEventListener('keypress', keyPress);
+        document.addEventListener('keydown', keyDown);
+        document.addEventListener('keyup', keyUp);
     }
 }
 
