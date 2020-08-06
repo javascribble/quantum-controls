29b264c1e912c3c6a1e6e7042507267736fca9d3
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-08-06 09:29:11 -0400

    remove broker adapter

--- source/main.js
+++ source/main.js
@@ -1,11 +1,7 @@
-import * as brokerAdapters from './adapters/broker.js';
 import * as keyboardUtilities from './utilities/keyboard.js';
 
-
 window.quantum = Object.assign(window.quantum || {}, {
-    ...brokerAdapters,
     ...keyboardUtilities
 });
 
-export * from './adapters/broker.js';
 export * from './utilities/keyboard.js';
--- test/debug.js
+++ test/debug.js
@@ -1,5 +1,5 @@
 import '/node_modules/@javascribble/quantum/source/main.js';
-import { Keyboard, keyboardBrokerAdapter } from '/source/main.js';
+import { Keyboard } from '/source/main.js';
 
 const keyboard = new Keyboard();
 keyboard.subscribe();
@@ -7,35 +7,5 @@ keyboard.keys.set(' ', {
     up: console.log,
     down: console.log
 });
-
-const configuration = {
-    defaultSchemata: [0],
-    schemata: [
-        {
-            name: 'test',
-            keys: [
-                {
-                    name: 'Enter',
-                    handlers: [
-                        {
-                            name: 'up',
-                            delegate: 'EnterUp',
-                        },
-                        {
-                            name: 'down',
-                            delegate: 'EnterDown'
-                        }
-                    ]
-                }
-            ]
-        }
-    ]
-};
-
-const broker = new quantum.EventBroker();
-broker.subscribe('EnterUp', console.log);
-broker.subscribe('EnterDown', console.log);
-
-keyboardBrokerAdapter.adapt(keyboard, broker, configuration);
 
 document.body.style.visibility = 'visible';
