efc7b92228bd6ed63b0a9210e242768aa0b20135
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-07-08 12:52:09 -0400

    update test

--- test/debug.js
+++ test/debug.js
@@ -1,6 +1,11 @@
 import '/node_modules/@javascribble/quantum/source/main.js';
 import '/source/main.js';
 
+import '/source/plugins/gamepad.js';
+import '/source/plugins/keyboard.js';
+import '/source/plugins/mouse.js';
+import '/source/plugins/touch.js';
+
 const input = document.querySelector('quantum-input');
 
 const { gamepad, keyboard, mouse, touch } = input.devices;
