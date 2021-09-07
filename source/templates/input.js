6ef774752478ae61d092977eb7394c3600c65a8a
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-09-07 12:36:20 -0400

    update style

--- source/templates/input.js
+++ source/templates/input.js
@@ -2,9 +2,11 @@ export default `
 <style>
     :host {
         position: absolute;
-        width: 100%;
-        height: 100%;
         outline: none;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
     }
 </style>
 `;
